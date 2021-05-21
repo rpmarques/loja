d2a717445dd42b2167de4382c3768fbbd315225c
Ricardo Pereira Marques <ricardo80@gmail.com>
2021-05-21 11:59:06 -0300

    Página minha-conta

---
+++ minha-conta.php
@@ -0,0 +1,159 @@
+<?php 
+require_once './header.php'
+?>
+    <div id="all">
+      <div id="content">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <!-- breadcrumb-->
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="./index.php">Home</a></li>
+                  <li aria-current="page" class="breadcrumb-item active">Minha Conta</li>
+                </ol>
+              </nav>
+            </div>
+            <div class="col-lg-3">
+              <!--
+              *** CUSTOMER MENU ***
+              _________________________________________________________
+              -->
+              <div class="card sidebar-menu">
+                <div class="card-header">
+                  <h3 class="h4 card-title">Seção do cliente</h3>
+                </div>
+                <div class="card-body">
+                  <ul class="nav nav-pills flex-column">
+                    <a href="./meus-pedidos.php" class="nav-link active"><i class="fa fa-list"></i> Meus Pedidos</a>
+                    <a href="./lista-desejos.php" class="nav-link"><i class="fa fa-heart"></i> Lista de Desejos</a>
+                    <a href="./minha-conta.php" class="nav-link"><i class="fa fa-user"></i> Minha Conta</a>
+                    <a href="/.logout.php" class="nav-link"><i class="fa fa-sign-out"></i> Sair</a></ul>
+                </div>
+              </div>
+              <!-- /.col-lg-3-->
+              <!-- *** CUSTOMER MENU END ***-->
+            </div>
+            <div class="col-lg-9">
+              <div class="box">
+                <h1>Minha Conta</h1>
+                <p class="lead">Altere seus dados pessoais ou sua senha aqui.</p>
+                <!-- <p class="text-muted">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p> -->
+                <h3>Alterar a senha</h3>
+                <form>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="password_old">Senha antiga</label>
+                        <input id="password_old" type="password" class="form-control">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="password_1">Senha nova</label>
+                        <input id="password_1" type="password" class="form-control">
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="password_2">Digite novamente a nova senha</label>
+                        <input id="password_2" type="password" class="form-control">
+                      </div>
+                    </div>
+                  </div>
+                  <!-- /.row-->
+                  <div class="col-md-12 text-center">
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Gravar Nova Senha</button>
+                  </div>
+                </form>
+                <h3 class="mt-5">Detalhes pessoais</h3>
+                <form>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="firstname">Nome</label>
+                        <input id="firstname" type="text" class="form-control">
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="lastname">Sobrenome</label>
+                        <input id="lastname" type="text" class="form-control">
+                      </div>
+                    </div>
+                  </div>
+                  <!-- /.row-->
+                  <div class="row">                    
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="street">Endereço</label>
+                        <input id="street" type="text" class="form-control">
+                      </div>
+                    </div>
+                    <div class="col-md-2">
+                      <div class="form-group">
+                        <label for="numero">Número</label>
+                        <input id="numero" type="text" class="form-control">
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="form-group">
+                        <label for="bairro">Bairro</label>
+                        <input id="bairro" type="text" class="form-control">
+                      </div>
+                    </div>
+                  </div>
+                  <!-- /.row-->
+                  <div class="row">
+                    <div class="col-md-6 col-lg-3">
+                      <div class="form-group">
+                        <label for="city">Cidade</label>
+                        <input id="city" type="text" class="form-control">
+                      </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                      <div class="form-group">
+                        <label for="zip">CEP</label>
+                        <input id="zip" type="text" class="form-control">
+                      </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                      <div class="form-group">
+                        <label for="state">Estado (UF)</label>
+                        <select id="state" class="form-control"></select>
+                      </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                      <div class="form-group">
+                        <label for="country">País</label>
+                        <select id="country" class="form-control"></select>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="phone">Telefone</label>
+                        <input id="phone" type="text" class="form-control">
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="email">Email</label>
+                        <input id="email" type="text" class="form-control">
+                      </div>
+                    </div>
+                    <div class="col-md-12 text-center">
+                      <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Salvar alterações</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php 
+    require_once './footer.php';
+    ?>
