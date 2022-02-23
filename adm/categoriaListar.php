489548f0178e67417ad5709f5ec3b9fbfb64fb48
Ricardo Pereira Marques <ricardo80@gmail.com>
2022-02-23 16:17:25 -0300

    crud categoria adm loja

---
+++ adm/categoriaListar.php
@@ -0,0 +1,50 @@
+<?php
+require_once './header.php';
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header"> </section>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Listagem de Categorias</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example1" class="table table-bordered table-striped table-sm">
+                                <thead>
+                                    <tr>
+                                        <th>Nome</th>
+                                        <th>Ação</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $categorias = $objProdutos->listaCategorias();
+                                    foreach ($categorias as $cat) { ?>
+                                        <tr>
+                                            <td><?= $cat->nome; ?></td>
+                                            <td>
+                                                <a class="btn bg-gradient-primary btn-xs" href="./catEditar.php?id=<?= base64_encode($cat->id) ?>"><i class="fa fa-edit"></i> Editar </a>
+                                                <a class="btn bg-gradient-danger btn-xs" href="./catExcluir.php?id=<?= base64_encode($cat->id) ?>"><i class="fa fa-eraser"></i> Exluir </a>
+                                            </td>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div> <!-- /.card-body -->
+                        <div class="card-footer">
+                            <a href="./catIncluir.php" class="btn btn-primary btn-sm">Cadastrar Marca</a>
+                        </div>
+                    </div> <!-- /.card -->
+                </div> <!-- /.col -->
+            </div> <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section><!-- /.content -->
+</div> <!-- /.content-wrapper -->
+<?php require_once './footer.php'; ?>
